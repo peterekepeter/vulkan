e83d99e85172d44adacc3a56748521503818981c
Eke Péter <peterekepeter@gmail.com>
2020-10-15 13:52:13 +0300

    Adds builtin shader support via win32 resources & GH workflows (#4)
    * Adds builtin shader support via win32 resources.
    
    * Also adds automatic builds support through github workflows

---
+++ vulkan/BuiltinShaders.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include "BuiltinShaders.h"
+#include <Windows.h>
+#include "resource.h"
+
+BuiltinShaders::BuiltinShaders()
+    : m_h_instance(GetModuleHandle(NULL))
+{
+}
+
+const BuiltinShaders::BinaryData& BuiltinShaders::get_fullscreen_triangle_vert_shader()
+{
+    return get_cached_resource(FULLSCREEN_TRIANGLE_VERT_RESOURCE);
+}
+
+const BuiltinShaders::BinaryData& BuiltinShaders::get_cached_resource(int id)
+{
+    using std::get;
+    for (auto& entry : m_cache) {
+        if (get<0>(entry) == id) {
+            return get<1>(entry);
+        }
+    }
+    m_cache.emplace_back(id, load_resource(id));
+    return get<1>(m_cache.back());
+}
+
+BuiltinShaders::BinaryData BuiltinShaders::load_resource(int id)
+{
+    HRSRC res = FindResource(m_h_instance, MAKEINTRESOURCE(id), RT_RCDATA);
+    if (!res)
+        throw std::runtime_error("resource " + std::to_string(id) + " not found");
+    HGLOBAL res_handle = LoadResource(NULL, res);
+    if (!res_handle)
+        throw std::runtime_error("resource " + std::to_string(id) + " failed to load");
+    char* res_data = (char*)LockResource(res_handle);
+    DWORD res_size = SizeofResource(NULL, res);
+    return BinaryData{ res_data, res_size };
+}
---
+++ vulkan/fullscreen-triangle.vert
@@ -0,0 +1,31 @@
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+layout (binding = 0) uniform UniformBufferObject {
+	vec2 resolution;
+	float time;
+	float reserved;
+} ubo;
+
+out gl_PerVertex {
+    vec4 gl_Position;
+};
+
+// draw a single triangle which covers the whole screen
+
+vec2 positions[3] = vec2[](
+    vec2(-3, -1),
+    vec2(+1, -1),
+    vec2(+1, +3)
+); 
+
+void main() {
+	#define REJECT() {gl_Position=vec4(-4,-4,-4,1);return;} 
+	int vid = gl_VertexIndex;
+	if (vid >= 3) REJECT();
+	int tid = vid/3; // triangle id
+	int pid = tid/2; // polygon id
+	int issecondtris = tid%2;
+	vec2 uv = positions[vid%3+issecondtris*2];
+	gl_Position = vec4(uv, 0.0, 1.0);
+}
